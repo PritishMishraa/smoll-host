3f5288450e641a92b2e576feff03b1f07809aa06
Pritish Mishra <pritishmishra579@gmail.com>
2024-05-12 00:14:38 +0530

    feat: Update Redis configuration with new URL and token

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,3 +1,6 @@
 import { Redis } from "@upstash/redis";
 
-export const redis: Redis = Redis.fromEnv();
+export const redis = new Redis({
+    url: "https://us1-probable-viper-42231.upstash.io",
+    token: "AaT3ASQgMTU2ZDkwMzAtY2UzYy00Y2JhLTg4OWQtZTVhZWNiZDgyYjYxNDc4MGZiZjI3MzlhNGU2MDk2Yzc1ZTc4ZGU2ZmU1YzY=",
+});
